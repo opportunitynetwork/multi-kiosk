91da863a7c641eec6a06fd6b96cf4c106bbd2275
Jose Augusto Paiva <86292099+jaugustoon@users.noreply.github.com>
2023-04-27 15:26:40 +0200

    Turn on dark mode

--- pkg/kiosk/newrelic_login.go
+++ pkg/kiosk/newrelic_login.go
@@ -62,16 +62,18 @@ func NewRelicKiosk(cfg *Config, messages chan string) {
 		panic(err)
 	}
 
+	time.Sleep(10000 * time.Millisecond)
+
 	if err := chromedp.Run(taskCtx,
-		chromedp.WaitVisible(`//div[class*=\"-UserArea-name\"]`, chromedp.BySearch),
-		chromedp.Click(`//div[class*=\"-UserArea-name\"]`, chromedp.BySearch),
+		chromedp.WaitVisible(`//button[aria-label="User menu"]`, chromedp.BySearch),
+		chromedp.Click(`//button[aria-label="User menu"]`, chromedp.BySearch),
 	); err != nil {
 		panic(err)
 	}
 
 	if err := chromedp.Run(taskCtx,
-		chromedp.WaitVisible(`//button[aria-label=\"Dark\"]`, chromedp.BySearch),
-		chromedp.Click(`//button[aria-label=\"Dark\"]`, chromedp.BySearch),
+		chromedp.WaitVisible(`//button[aria-label="Dark"]`, chromedp.BySearch),
+		chromedp.Click(`//button[aria-label="Dark"]`, chromedp.BySearch),
 	); err != nil {
 		panic(err)
 	}
